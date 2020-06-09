fc1410d9eef84e2f6baee31176bc05c75709188f
brunomarcelino30 <brunomarcelino30>
2020-06-09 20:37:28 -0300

    Apresentando erro

---
+++ ExercicioMetodoAbstrato/Entities/Shape.cs
@@ -0,0 +1,17 @@
+﻿using ExercicioMetodoAbstrato.Entities.Enums;
+
+
+namespace ExercicioMetodoAbstrato.Entities
+{
+    abstract class Shape
+    {
+        public Color Color { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
+    }
+}
---
+++ ExercicioMetodoAbstrato/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Globalization;
+using ExercicioMetodoAbstrato.Entities;
+using ExercicioMetodoAbstrato.Entities.Enums;
+
+namespace ExercicioMetodoAbstrato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> list = new List<Shape>();
+
+            Console.Write("Enter the number of shapes: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine($"Shape #{i} data:");
+                Console.Write("Rectangle or Circle (r/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Color (Black/Blue/Red): ");
+                //Color color = Enum.Parse<Color>(Console.ReadLine());
+            }
+        }
+    }
+}
